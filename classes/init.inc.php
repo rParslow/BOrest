6154ba47279f39bdf98d17ad11cd9a3744a0553d
rParslow <iorek@m4p.li>
2017-03-01 22:59:16 +0100

    init script

---
+++ classes/init.inc.php
@@ -0,0 +1,66 @@
+<?PHP
+
+define("DATABASE_FILE", "./database/payzen.sqlite");
+
+
+// Create the IPN table 
+//
+function create_ipn_table() {
+  $db  = new SQLite3(DATABASE_FILE);
+  $db->exec(
+    "CREATE TABLE IF NOT EXISTS ipn (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    status STRING,
+
+    orderId STRING,
+    xType STRING,
+    transactions BLOB,
+
+    id0 STRING,
+    lastUpdateDate0 STRING,
+
+    full BLOB,
+ 
+    checked STRING
+    )"
+  );
+
+  $db->exec(
+    "CREATE TABLE IF NOT EXISTS payments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+
+    status STRING,
+    pType STRING,
+    applicationVersion STRING,
+    webService STRING,
+    applicationProvider STRING,
+    version STRING, 
+
+    isTest STRING,
+    shopId STRING,
+    amount STRING,
+    parentBillingTransaction STRING,
+    detailedErrorCode STRING,
+
+    answer BLOB,
+
+    paymentMethod BLOB, 
+    paymentMethodDetails BLOB,
+    customer BLOB,
+    additionalInfo BLOB,
+
+    serverDateTime STRING,
+    ticket STRING,
+    metadata STRING 
+    )"
+  );
+  echo DATABASE_FILE." created<br />";
+
+
+// create_ipn_table 
+}
+
+
+
