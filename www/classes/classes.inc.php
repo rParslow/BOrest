f7839f3d8613a54c8b2b19827c34441c05c78f28
rParslow <iorek@m4p.li>
2017-03-02 19:58:11 +0100

    classes

---
+++ www/classes/classes.inc.php
@@ -0,0 +1,19 @@
+<?PHP
+
+/////////////////////////////////////////////
+// Classes used for 
+//
+//  - Constant names 
+//  - Tables creations
+//  - TinyButStrong functions 
+//
+/////////////////////////////////////////////
+
+
+// Variables & Constants
+//
+
+define("OBFUSCATION","YES"); 
+define("DATABASE_FILE", "./database/payzen.sqlite");
+
+
